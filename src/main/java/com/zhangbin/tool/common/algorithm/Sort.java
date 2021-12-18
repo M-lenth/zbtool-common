9935e893024b7ad30bb23bfceb563d23f891ab0a
zhangbin1 <1812606603@qq.com>
2021-12-18 18:31:23 +0800

    排序工具类

--- src/main/java/com/zhangbin/tool/common/algorithm/Sort.java
+++ src/main/java/com/zhangbin/tool/common/algorithm/Sort.java
@@ -10,7 +10,33 @@ package com.zhangbin.tool.common.algorithm;
  */
 public class Sort {
 
-    public static <T> void merge(){
+    /**
+     * 归并排序入口
+     *
+     * @param sort 比较的数组，需要实现接口{@link Comparable}
+     * @param <T>  比较的类型
+     */
+    public static <T> T[] merge(Comparable<T>[] sort) {
 
+        return null;
+    }
+
+    /**
+     * 一趟归并
+     *
+     * @param sort 排序目标数组
+     * @param low  低位指针
+     * @param mid  中间点
+     * @param high 高位指针
+     * @param <T>  排序类型
+     */
+    private static <T> T[] mergeSort(Comparable<T> sort, int low, int mid, int high) {
+
+        return null;
+    }
+
+    public static <T> T[] quick() {
+
+        return null;
     }
 }
