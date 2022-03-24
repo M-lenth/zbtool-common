02141115f9811cfe2d7adc1c9a7a4fcb274952bc
zhangbin1 <zhangbin1@baiwang.com>
2022-03-24 15:34:02 +0800

    Map转换方法

--- src/main/java/com/zhangbin/tool/common/util/BeanUtils.java
+++ src/main/java/com/zhangbin/tool/common/util/BeanUtils.java
@@ -3,6 +3,7 @@ package com.zhangbin.tool.common.util;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * ClassName: BeanUtils <br>
@@ -22,8 +23,7 @@ public class BeanUtils {
      * @param args       参数列表
      * @return 方法返回值
      */
-    public static Object invoke(Object obj, String methodName, Class<?>[] argsType, Object[] args)
-        throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static Object invoke(Object obj, String methodName, Class<?>[] argsType, Object[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Class<?> aClass = obj.getClass();
         Method method = aClass.getDeclaredMethod(methodName, argsType);
         method.setAccessible(true);
@@ -37,8 +37,7 @@ public class BeanUtils {
      * @param methodName 目标方法
      * @return 调用的目标方法返回值
      */
-    public static Object invoke(Object obj, String methodName)
-        throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    public static Object invoke(Object obj, String methodName) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         return invoke(obj, methodName, null, null);
     }
 
@@ -49,8 +48,7 @@ public class BeanUtils {
      * @param tClass 目标对象的类型
      * @return 复制后目标对象的值
      */
-    public static <T> T copyProperties(Object obj, Class<T> tClass)
-        throws InstantiationException, IllegalAccessException {
+    public static <T> T copyProperties(Object obj, Class<T> tClass) throws InstantiationException, IllegalAccessException {
         T t = tClass.newInstance();
         Class<?> tmpClass = obj.getClass();
         while (tmpClass != null) {
@@ -68,4 +66,42 @@ public class BeanUtils {
         }
         return t;
     }
+
+    /**
+     * 将对象转换为Map
+     *
+     * @param obj 转换的对象
+     * @return 转换后的Map
+     */
+    public static Map<String, Object> objectToMap(Object obj) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        Class<?> tmpClass = obj.getClass();
+        while (tmpClass != null) {
+            for (Field field : tmpClass.getDeclaredFields()) {
+                try {
+                    map.put(field.getName(), field.get(obj));
+                } catch (IllegalAccessException ignored) {
+                }
+            }
+            tmpClass = tmpClass.getSuperclass();
+        }
+        return map;
+    }
+
+    /**
+     * 把配置文件转换为Map对象
+     *
+     * @param properties 配置文件 {@link Properties}
+     * @return 转换后的Map对象
+     */
+    public static Map<String, Object> propToMap(Properties properties) {
+        Set<Object> objects = properties.keySet();
+        Map<String, Object> map = new LinkedHashMap<>();
+        for (Object o : objects) {
+            Object o1 = properties.get(o);
+            map.put((String) o, o1);
+        }
+        return map;
+    }
+
 }
