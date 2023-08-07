1f05e08088e8237038500241274a036ece6208fc
zhangbin <zb00910@163.com>
2023-08-07 16:51:53 +0800

    删除net模块

--- src/main/java/pers/zhangbin/tool/ToolTest.java
+++ src/main/java/pers/zhangbin/tool/ToolTest.java
@@ -1,11 +1,5 @@
 package pers.zhangbin.tool;
 
-import com.alibaba.fastjson.JSON;
-import pers.zhangbin.tool.io.Resource;
-import pers.zhangbin.tool.net.HttpRequest;
-import pers.zhangbin.tool.net.HttpResponse;
-import pers.zhangbin.tool.net.HttpUtils;
-
 /**
  * ClassName: ToolTest <br>
  * Time 2023/6/13 23:39
@@ -16,18 +10,5 @@ import pers.zhangbin.tool.net.HttpUtils;
  */
 public class ToolTest {
 
-    public static void main(String[] args) throws Exception {
-//        InputStream is = new Resource("tst.txt").getResourceAsInputStream();
-//        String string = StringUtils.getStringByInStream(is, StandardCharsets.UTF_8);
-//        System.out.println(string);
-//        HttpResponse https = HttpUtils.executeGetHttpsNotSSL(url);
-//        System.out.println(response.getData());
-//        HttpResponse http = HttpUtils.executeGetHttp("http://localhost:8080/test/post_obj");
-//        HttpRequest request = new HttpRequest.Builder().post(JSON.toJSONString(test)).url("http://localhost:8080/test/post_obj").build();
-//        HttpRequest request = new HttpRequest.Builder().put(JSON.toJSONString(test)).url("http://localhost:8080/test/put_obj").build();
-//        HttpRequest request = new HttpRequest.Builder().delete(JSON.toJSONString(test)).url("http://localhost:8080/test/delete_obj").build();
-//
-//        HttpResponse response = HttpUtils.executeHttp(request);
-//        System.out.println(JSON.toJSONString(response));
-    }
+    public static void main(String[] args) { }
 }
